d0f78d3d35657c6a202177d3348a7bd81a202c69
vaseem <manuknl2@gmail.com>
2024-12-10 21:41:25 +0530

    'start'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RegComponent } from './reg/reg.component';
+import { LoginComponent } from './login/login.component';
+import { ViewComponent } from './view/view.component';
+import { SavedComponent } from './saved/saved.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'',component:HomeComponent },
+    {path:'abt',component:AboutComponent },
+    {path:'contact',component:ContactComponent },
+    {path:'login',component:LoginComponent },
+    {path:'reg',component:RegComponent },
+    {path:'view',component:ViewComponent },
+    {path:'saved',component:SavedComponent },
+    {path:'recipe',component:RecipeComponent },
+    {path:'**',redirectTo:''}
+];
---
+++ src/app/reg/reg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg',
+  standalone: true,
+  imports: [],
+  templateUrl: './reg.component.html',
+  styleUrl: './reg.component.css'
+})
+export class RegComponent {
+
+}
